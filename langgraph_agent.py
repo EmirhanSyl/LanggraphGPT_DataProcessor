7a1bde2002a4e09d5dfb233b26536da99f3ef44b
Emirhan Soylu <61618968+EmirhanSyl@users.noreply.github.com>
2024-08-09 03:44:54 +0300

    Update langgraph_agent.py

--- langgraph_agent.py
+++ langgraph_agent.py
@@ -2,12 +2,17 @@ from langchain_ollama import ChatOllama
 from langchain_core.messages import AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 
+db = [{
+    'username': 'ahmet',
+    'password': '123',
+}]
+
 translator = ChatOllama(
     model="aya",
     temperature=0,
 )
 
-prompt = ChatPromptTemplate.from_messages(
+translator_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -17,14 +22,53 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-chain = prompt | translator
-aimessage = chain.invoke(
+chain = translator_prompt | translator
+translated_msg = chain.invoke(
     {
         "input_language": "Turkish",
         "output_language": "English",
-        "input": "Veri setimde bulunan flight_times isimli kolonu datetime türüne dönüştürüp bu kolona bir time shifting işlemi uygulamak istiyorum",
+        "input": "kullanıcı adı 'ahmet' ve şifresi '123' olan bir kullanıcının varlığını doğrular mısın?",
     }
 )
 
-print(aimessage)
+print(translated_msg.content)
+
+
+def validate_user(username: str, password: str) -> bool:
+    """Validate user using username and password.
+
+    Args:
+        username: (int) the username of the user.
+        password: (str) user's password.
+    """
+    for entry in db:
+        if entry['username'] == username and entry['password'] == password:
+            return True
+    return False
+
+
+llm = ChatOllama(
+    model="llama3-groq-tool-use",
+    temperature=0,
+).bind_tools([validate_user])
+
+llm_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a password validators for the users. Validate Passwords and if user is valid, say 'holla!'",
+        ),
+        ("human", "{input}"),
+    ]
+)
+
+chain = llm_prompt | llm
+
+result = chain.invoke(
+    {
+        "input": translated_msg.content,
+    }
+)
+print(result.tool_calls)
+print(result.content)
 
