cd3dd6007727b870e9c7465320915ca58e4a29be
Emirhan Soylu <61618968+EmirhanSyl@users.noreply.github.com>
2024-08-14 15:51:14 +0300

    Update langgraph_hil.py

--- langgraph_hil.py
+++ langgraph_hil.py
@@ -1,9 +1,11 @@
-from typing import Annotated
+import json
+import uuid
+from typing import Annotated, Optional
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
 from langchain.tools import tool
-from langgraph.prebuilt import ToolExecutor, ToolInvocation
-from langchain_core.messages import ToolMessage, HumanMessage
+from langgraph.prebuilt import ToolExecutor, ToolInvocation, ToolNode
+from langchain_core.messages import ToolMessage, HumanMessage, AIMessage
 from langchain_ollama import ChatOllama
 from langgraph.graph import END, StateGraph, START
 from langgraph.checkpoint.memory import MemorySaver
@@ -62,7 +64,7 @@ def handle_missing_values(column_name: str) -> str:
     """
     mean_value = df[column_name].mean()
     missing_count = df[column_name].isna().sum()
-    df[column_name].fillna(mean_value, inplace=True)
+    df[column_name] = df[column_name].fillna(mean_value)
 
     return (f"The mean value for '{column_name}' is {mean_value:.2f}. "
             f"Replaced {missing_count} missing values with this mean.")
@@ -118,6 +120,37 @@ def call_tool(state):
     return {"messages": [tool_message]}
 
 
+# Helper function to construct message asking for verification
+def generate_verification_message(message: AIMessage) -> AIMessage:
+    """Generate "verification message" from message with tool calls."""
+    serialized_tool_calls = json.dumps(
+        message.tool_calls,
+        indent=2,
+    )
+    return AIMessage(
+        content=(
+            "I plan to invoke the following tools, do you approve?\n\n"
+            "Type 'y' if you do, anything else to stop.\n\n"
+            f"{serialized_tool_calls}"
+        ),
+        id=message.id,
+    )
+
+
+# Helper function to stream output from the graph
+def stream_app_catch_tool_calls(inputs, thread) -> Optional[AIMessage]:
+    """Stream app, catching tool calls."""
+    tool_call_message = None
+    for event in app.stream(inputs, thread, stream_mode="values"):
+        message = event["messages"][-1]
+        if isinstance(message, AIMessage) and message.tool_calls:
+            tool_call_message = message
+        else:
+            message.pretty_print()
+
+    return tool_call_message
+
+
 workflow = StateGraph(State)
 workflow.add_node("agent", call_model)
 workflow.add_node("action", call_tool)
@@ -143,7 +176,41 @@ workflow.add_edge("action", "agent")
 memory = MemorySaver()
 
 app = workflow.compile(checkpointer=memory, interrupt_before=["action"])
-thread = {"configurable": {"thread_id": "2"}}
-inputs = [HumanMessage(content="can you fill the missing values on the 'CAUSE' column in the dataset?")]
-for event in app.stream({"messages": inputs}, thread, stream_mode="values"):
-    event["messages"][-1].pretty_print()
+# thread = {"configurable": {"thread_id": "2"}}
+inputs = [HumanMessage(content="can you fill the missing values on the column named 2019 in the dataset?")]
+# for event in app.stream({"messages": inputs}, thread, stream_mode="values"):
+#    event["messages"][-1].pretty_print()
+
+thread = {"configurable": {"thread_id": "3"}}
+tool_call_message = stream_app_catch_tool_calls(
+    {"messages": inputs},
+    thread,
+)
+
+while tool_call_message:
+    verification_message = generate_verification_message(tool_call_message)
+    verification_message.pretty_print()
+    input_message = HumanMessage(input())
+    if input_message.content == "exit":
+        break
+    input_message.pretty_print()
+
+    # First we update the state with the verification message and the input message.
+    # note that `generate_verification_message` sets the message ID to be the same
+    # as the ID from the original tool call message. Updating the state with this
+    # message will overwrite the previous tool call.
+    snapshot = app.get_state(thread)
+    snapshot.values["messages"] += [verification_message, input_message]
+
+    if input_message.content == "y":
+        tool_call_message.id = str(uuid.uuid4())
+        # If verified, we append the tool call message to the state
+        # and resume execution.
+        snapshot.values["messages"] += [tool_call_message]
+        app.update_state(thread, snapshot.values, as_node="agent")
+    else:
+        # Otherwise, resume execution from the input message.
+        app.update_state(thread, snapshot.values, as_node="__start__")
+
+    tool_call_message = stream_app_catch_tool_calls(None, thread)
+
